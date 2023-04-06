0873f22ea24a243edb86de6af79249404817e87e
yourspleenfell <37151536+yourspleenfell@users.noreply.github.com>
2023-04-06 12:15:13 -0700

    Initial Commit
    Initial Commit
    Diagonal Difference

---
+++ April/DiagonalDiff.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    l2r = 0
+    r2l = 0
+    length = len(arr)
+    for x in range(length):
+        l2r += arr[x][x]
+        r2l += arr[x][length - x - 1]
+    return abs(l2r - r2l)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
